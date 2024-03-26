eac2c909ecdd499e40488c2967b826cf13579c08
Hussein380 <huznigarane@gmail.com>
2024-03-26 17:58:28 +0300

    corrected a typo

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -17,5 +17,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		func(tree->n);
 		/*traverse the right side of te tree*/
 		binary_tree_inorder(tree->right, func);
+	}
 
 }
