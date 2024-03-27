167ad48ca9846ca346622658284b428c9e38eb2c
Hussein380 <huznigarane@gmail.com>
2024-03-27 14:44:35 +0300

    corrected a typo

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		/*Initialize variable to store the height of the left and right subtree*/
-		size_t Left = 0, right = 0;
+		size_t left = 0, right = 0;
 		/*if the left child of the current node exist,*/
 		 /*calculate it height recusively and add 1 for the current node*/
 		left = tree->left ? 1 + binary_tree_height(tree->left) : 0;
